c3b96b14150a29f1f5047b09b73aeb8575d84b14
tsutomu <matsuo0215@gmail.com>
2014-10-13 15:50:39 +0900

    finish signin-out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Yin::Application.routes.draw do
   #get "users/new"
   #get "static_pages/home"
   resources:users
+  resources:sessions,only:[:new,:create,:destroy]
   root 'static_pages#home'
+  match '/signin' ,to:'sessions#new' ,via:'get'
+  match '/signout' ,to:'sessions#destroy' ,via:'delete'
   match '/signup' ,to:'users#new' ,via:'get'
   match '/help', to:'static_pages#help', via:'get'
   match '/about', to:'static_pages#about', via:'get'
